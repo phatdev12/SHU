03a85544443ad47192582768f1c7c9b557e1c6c5
徐胜發 <tuphat3456tt@gmail.com>
2025-11-06 23:27:58 +0700

    feat: first demo home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,32 @@
+import { Feature } from "@/components/pages/home/feature";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="relative flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="relative top-0 left-0 w-full h-screen overflow-hidden">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/hero.svg"
+          fill
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover object-top"
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+      <div className="absolute aspect-square lg:w-4/6 w-full bg-dark-blue rounded-full top-100 flex items-center justify-center p-22" style={{
+        boxShadow: '0 0 60px 10px rgba(0, 16, 197)',
+      }}>
+        <div className="aspect-square w-full bg-linear-to-b from-dark-blue to-light-blue rounded-full flex items-center justify-center">
+          <span className="relative bottom-50 text-[300px] font-bold text-white">30</span>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div className="absolute min-h-screen w-10/12 flex flex-col lg:flex-row px-10">
+        <h1 className="relative top-45 lg:w-1/2 w-full text-7xl font-bold text-white">PROTECT OUR EARTH TOGETHER</h1>
+        <h1 className="relative top-50 lg:w-1/2 w-full text-7xl font-bold text-white text-right">CLEAN AIR, CLEAN FUTURE</h1>
+      </div>
+      
+      
+      <Feature />
     </div>
   );
 }
---
+++ components/pages/home/feature.tsx
@@ -0,0 +1,7 @@
+export function Feature() {
+  return (
+    <div className="relative flex w-full min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      
+    </div>
+  );
+}
